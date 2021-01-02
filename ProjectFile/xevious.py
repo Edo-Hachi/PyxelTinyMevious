620ea1adef6e4ccae254d0598950357165ab8018
Edohachi-nekoya <edohachi.nekoya@gmail.com>
2021-01-02 21:02:29 +0900

    Update xevious.py
    ゲームステート整備

--- ProjectFile/xevious.py
+++ ProjectFile/xevious.py
@@ -2,25 +2,27 @@ import pyxel
 import define
 import enemy
 
-bullet_list = []    #ザッパー管理リスト
-
+#------------------------------------------------------------------------------
 # グローバル変数
-#_VSYNC = 0
-
+bullet_list = []    #ザッパー管理リスト
 enemy_list = []    #敵管理リスト
 
+_VSYNC = 0
+
+#------------------------------------------------------------------------------
 # #線形リストオブジェクトへのupdate一括処理
 def update_list(list):
     for elem in list:
         elem.update()
 
+#------------------------------------------------------------------------------
 #線形リストオブジェクトへのdraw一括処理
 def draw_list(list, vsync):
     for elem in list:
         elem.draw(vsync)
-        
 
-#線形リストオブジェクト        
+#------------------------------------------------------------------------------
+#線形リストオブジェクトメンバ破棄
 def flash_list(list):
     i = 0
     while i < len(list):
@@ -31,8 +33,8 @@ def flash_list(list):
         else:
             i += 1
 
-
-#ザッパー管理クラス
+#------------------------------------------------------------------------------
+#自弾管理クラス
 class Bullet:
     def __init__(self, x, y):
         self.x = x
@@ -59,41 +61,36 @@ class Bullet:
             pyxel.blt(self.x, self.y, 0, 0, 32, define.BULLET_WIDTH, define.BULLET_HEIGHT, define.MASK_COLOR)
         else:
             pyxel.blt(self.x, self.y, 0, 8, 32, define.BULLET_WIDTH, define.BULLET_HEIGHT, define.MASK_COLOR)
-        
 
-
-class Gump_main:
-    
-
-    def __init__(self):
-        # 初期化
-
-
-        pyxel.init(255, 255, caption="Xevious",
-        #                    0         1         2         3         4         5         6         7         8         9         10        11        12        13        14        15
-                    palette=[0x000000, 0x8CC323, 0x69B923, 0x007846, 0xF0EB3C, 0x194696, 0x7D7D7D, 0xE61414, 0xFFFFFF, 0x824141, 0xC8AA32, 0x000000, 0x000000, 0x000000, 0x000000, 0xC896B4],
-                    fps = 60,  quit_key=pyxel.KEY_Q)
-
-        #pyxel.init(255, 255, caption="Xevious", fps=60, quit_key=pyxel.KEY_Q)
-        pyxel.load("./assets/xevious.pyxres")
-        
-        pyxel.image(0).load(0, 0, "./assets/xevious_01.png")
-        pyxel.image(1).load(0, 0, "./assets/xevious_bg.png")
-
+#------------------------------------------------------------------------------
+def _Update_Title(self):
+    if pyxel.btn(pyxel.KEY_1):
+        self.GameState = define.STATE_PLAY
         self.Map_y = (255 - 32)
         self.y_offset = 8
         
-        self.px = 100   #自機の座標
-        self.py = 100
+        self.px = 128 - 8   #自機の座標
+        self.py = 200
 
         self.vsync = 0
 
-        # 実行
-        pyxel.run(self.update, self.draw)
+#------------------------------------------------------------------------------
+def _Draw_Title(self):
+    # 画面を消去
+    pyxel.cls(0)
+# 1cha = 4pix
+    txt = "Smell Like a Tiny XEVIOUS"
+    txtlen = len(txt) * 4
+    pyxel.text(128 - (txtlen /2), 50, txt, 14)
 
-    def update(self):
+    txt = "Press [1] to Start Game!"
+    txtlen = len(txt) * 4
+    pyxel.text(128 - (txtlen /2), 60, txt, 14)
+    #pyxel.text(123, 60, txt, 14)
 
-        self.vsync += 1
+#------------------------------------------------------------------------------
+def _Update_Play(self):
+        #self.vsync += 1
 
         if 59 <=  self.vsync:
             self.vsync = 0
@@ -142,17 +139,11 @@ class Gump_main:
         update_list(enemy_list)
         flash_list(enemy_list)
 
-
-    def draw(self):
-        # 描画
+#------------------------------------------------------------------------------
+def _Draw_Play(self):
+        # 描画\
         # 画面を消去
         pyxel.cls(0)
-        
-        #pyxel.tilemap(0).refimg = 1
-        
-        #pyxel.tilemap(0).copy(0, 0, 0, 16, 16, 0, 0)
-        
-        #pyxel.rect(0,0,100,100, 1)
 
         #背景表示
         pyxel.bltm(0,self.y_offset * -1, #実画面の表示原点
@@ -184,6 +175,38 @@ class Gump_main:
         temp = "PX= " + str(self.px) + ": PY=" + str(self.py)
         pyxel.text(0, 0, temp, 7)
 
+#------------------------------------------------------------------------------
+#ゲームメインループ
+class GameMain:
 
+    def __init__(self):
+        # 初期化
+        pyxel.init(255, 255, caption="Tiny Xevious",
+        #                    0         1         2         3         4         5         6         7         8         9         10        11        12        13        14        15
+                    palette=[0x000000, 0x8CC323, 0x69B923, 0x007846, 0xF0EB3C, 0x194696, 0x7D7D7D, 0xE61414, 0xFFFFFF, 0x824141, 0xC8AA32, 0x000000, 0x000000, 0x000000, 0xffffff, 0xC896B4],
+                    fps = 60,  quit_key=pyxel.KEY_Q)
 
-Gump_main()
+        #pyxel.init(255, 255, caption="Xevious", fps=60, quit_key=pyxel.KEY_Q)
+        pyxel.load("./assets/xevious.pyxres")
+        
+        pyxel.image(0).load(0, 0, "./assets/xevious_01.png")
+        pyxel.image(1).load(0, 0, "./assets/xevious_bg.png")
+
+        self.GameState = define.STATE_TITLE
+
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+
+        if self.GameState == define.STATE_PLAY:
+            _Update_Play(self)
+        elif self.GameState == define.STATE_TITLE:
+            _Update_Title(self)
+
+    def draw(self):
+        if self.GameState == define.STATE_PLAY:
+            _Draw_Play(self)
+        elif self.GameState == define.STATE_TITLE:
+            _Draw_Title(self)
+
+GameMain()
